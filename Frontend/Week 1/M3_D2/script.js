5f00f275b65fd1255f6b57ef7b3d46233f1bd162
VardanGalstian <81989128+VardanGalstyan@users.noreply.github.com>
2021-05-18 20:35:15 +0200

    Unhappy with the result

---
+++ Frontend/Week 1/M3_D2/script.js
@@ -0,0 +1,92 @@
+window.onload = () => {
+    fetchResult();
+    fetchResult1();
+    fetchResult2(); 
+}
+
+
+const fetchResult = () => {
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        
+        for (let i = 0; i < data.data.length; i++) {
+            let result = data.data[i]
+            const newCard = document.createElement("div")
+            newCard.classList.add("card")
+            document.querySelector(".container .row").appendChild(newCard)
+            const img = document.createElement("img")
+            img.classList.add("card-img-top")
+            img.src = result.album.cover
+            newCard.appendChild(img)
+
+            const element = data.data[i];
+            console.log(element);
+            
+        }
+        
+        
+    })
+
+    
+        
+}
+
+const fetchResult1 = () => {
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=behemoth")
+    .then(response => response.json())
+    .then(data => {
+       
+        for (let i = 0; i < data.data.length; i++) {
+            let result = data.data[i]
+            const newCard = document.createElement("div")
+            newCard.classList.add("card")
+            document.querySelector(".container .row").appendChild(newCard)
+            const img = document.createElement("img")
+            img.classList.add("card-img-top")
+            img.src = result.album.cover
+            newCard.appendChild(img)
+
+            const element = data.data[i];
+            console.log(element);
+            
+        }
+        
+        
+    })
+
+    
+        
+}
+
+
+const fetchResult2 = () => {
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        
+        for (let i = 0; i < data.data.length; i++) {
+            let result = data.data[i]
+            const newCard = document.createElement("div")
+            newCard.classList.add("card")
+            document.querySelector(".container .row").appendChild(newCard)
+            const img = document.createElement("img")
+            img.classList.add("image-fluid")
+            img.src = result.album.cover
+            newCard.appendChild(img)
+
+            const element = data.data[i];
+            console.log(element);
+            
+        }
+        
+        
+    })
+
+    
+        
+}
+
+
